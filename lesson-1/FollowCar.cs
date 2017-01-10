16a2ffea0f8d14bfabf76afcb7175fb7d5531394
creindle <creindle@hawaii.edu>
2017-01-10 12:49:13 -1000

    Scripts for Controlling the car and camera setup
    Will implement commented out code one day.

---
+++ lesson-1/FollowCar.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCar : MonoBehaviour {
+
+	public GameObject player;
+
+	private Vector3 offset;
+
+	public float turnSpeed = 4.0f;
+
+	// Use this for initialization
+	void Start () {
+		//offset = new Vector3 (player.transform.position.x + 2.0f, player.transform.position.y + 8.0f, player.transform.position.z + 7.0f);
+		offset = transform.position - player.transform.position;
+		//offset = transform.TransformPoint (Vector3.back * 2);
+		//Instantiate (player, offset, player.transform.rotation);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//offset = Quaternion.AngleAxis (Input.GetAxis ("Horizontal") * turnSpeed, Vector3.back) * offset;
+		transform.position = Vector3.Lerp (transform.position, player.transform.TransformPoint(offset), turnSpeed * Time.deltaTime);
+		//transform.position = player.transform.position + offset;
+		transform.LookAt (player.transform);
+		//transform.eulerAngles = new Vector3 (transform.eulerAngles.x, 0, 0);
+	}
+}
---
+++ lesson-1/MoveCar.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCar : MonoBehaviour {
+
+	public float speed;
+
+	public float angularSpeed = 2f;
+
+	private Rigidbody rb;
+
+	Vector3 rotationY;
+	Vector3 movement;
+
+	float newNumber;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+		
+	// Update is called once per frame
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertically = Input.GetAxis ("Vertical");
+
+		//Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertically);
+
+		MoveForward (moveVertically);
+		SteerCar (moveHorizontal);
+
+		//rb.AddForce (Vector3.forward * speed);
+	}
+
+	void SteerCar (float moveHorizontal) {
+		rotationY.Set (0f, moveHorizontal, 0f);
+		rotationY = rotationY.normalized * angularSpeed;
+
+		newNumber = rb.rotation.eulerAngles.y;
+
+		newNumber += moveHorizontal;
+
+		rb.MoveRotation(Quaternion.Euler(0, newNumber, 0));
+	}
+
+	void MoveForward (float moveVertically){
+		if (moveVertically > 0) {
+			movement = transform.forward;
+			movement = movement.normalized * speed * Time.deltaTime;
+			rb.MovePosition (rb.position + movement);
+		}
+		else if (moveVertically < 0) {
+			movement = transform.forward * -1;
+			movement = movement.normalized * (speed/2) * Time.deltaTime;
+			rb.MovePosition(rb.position + movement);
+		}
+	}
+}
